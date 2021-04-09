420ef1f4e2eef48355cd57cfa8c0b1bb7f44e73c
Minh Ngo <ngo.minh0224@gmail.com>
2021-04-09 09:57:41 -0700

    set up library

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require('./lib/app');
-const pool = require('./lib/utils/pool');
 
 const PORT = process.env.PORT || 7890;
 
@@ -10,5 +9,4 @@ app.listen(PORT, () => {
 
 process.on('exit', () => {
   console.log('Goodbye!');
-  pool.end();
 });
