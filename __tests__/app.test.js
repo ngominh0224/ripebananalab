aa2d40ae5fda8d154aee18584703d2054de8edbd
Minh Ngo <ngo.minh0224@gmail.com>
2021-04-09 15:45:01 -0700

    pull from master

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,22 +5,21 @@ const db = require('../lib/utils/database');
 
 describe('ripebanana routes', () => {
   beforeEach(() => {
-    return db.sync({force:true});
+    return db.sync({ force: true });
   });
-
 
   it('adds a new actor to the db', () => {
     const newActor = {
       name: 'Bob Loblaw',
       dob: '1984-04-15',
-      pob: 'Timbuktu'
-    }
+      pob: 'Timbuktu',
+    };
 
     return request(app)
       .post('/api/v1/actors')
       .send(newActor)
       .then((res) => {
-        expect(res.body).toEqual({id: 1, ...newActor});
-      })
-  })
+        expect(res.body).toEqual({ id: 1, ...newActor });
+      });
+  });
 });
